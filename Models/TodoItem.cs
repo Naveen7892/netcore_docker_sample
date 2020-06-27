83b9518b6972ec00e8d3b96686f051c5d5d9676d
Naveen Kumar <naveentere7@outlook.com>
2020-06-28 01:51:51 +0530

    Added Models and DbContext files and updated DI container

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace webapi_test.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set;}
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using webapi_test.Models;
 
 namespace webapi_test
 {
@@ -25,6 +27,7 @@ namespace webapi_test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
